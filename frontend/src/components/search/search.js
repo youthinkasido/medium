ddfe9b10921bba2e5d209483c2be3ec5b7e55000
Robert <robertyeakeltroidl@gmail.com>
2019-09-23 11:07:32 -0700

    add base styling to searchbar

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -1,36 +1,42 @@
-
-import React, { Component } from 'react'
-import './search.css'
+import React, { Component } from "react";
+import "./search.css";
 
 class Search extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            searchInput: '',
-            suggestions: [],
-            stories: []
-        }
-        this.update = this.update.bind(this)
-        // this.renderSuggestions = this.renderSuggestions.bind(this)
-    }
+    this.state = {
+      searchInput: "",
+      suggestions: [],
+      stories: []
+    };
+    this.update = this.update.bind(this);
+    // this.renderSuggestions = this.renderSuggestions.bind(this)
+  }
 
-    update(e, field) {
+  update(e, field) {
+    this.setState(
+      {
+        [field]: e.target.value,
+        suggestions: ""
+      },
+      () => this.props.fetchStories(this.state.searchInput)
+    );
+  }
 
-        this.setState({
-            [field]: e.target.value,
-            suggestions: ''
-        }, () => this.props.fetchStories(this.state.searchInput))
-    }
-
-    render() {
-        return (
-            <div className="search-bar">
-                <input type="text" onChange={(e) => this.update(e, 'searchInput')} value={this.state.searchInput} />
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          placeholder="search"
+          className="search-input"
+          type="text"
+          onChange={e => this.update(e, "searchInput")}
+          value={this.state.searchInput}
+        />
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
