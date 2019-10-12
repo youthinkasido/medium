b389f4e1b3609fb03004761fe1f9651f1314d35c
Robert <robertyeakeltroidl@gmail.com>
2019-10-12 13:55:41 -0700

    create skeleton for search results react component

---
+++ frontend/src/components/search_results/search_results.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class SearchResults extends Component {
+  render() {
+    return <div>search results here</div>;
+  }
+}
+
+export default withRouter(SearchResults);
